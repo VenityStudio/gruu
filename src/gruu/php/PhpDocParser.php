a3beea75918bbc758f4ccbe802e067f6a1e5f728
mwguy <yousan4ik@gmail.com>
2019-03-07 23:06:45 +0300

    Add PhpDocParser

---
+++ src/gruu/php/PhpDocParser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace gruu\php;
+
+
+use php\lib\str;
+
+class PhpDocParser
+{
+    /**
+     * @var string
+     */
+    private $input;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * PhpDocParser constructor.
+     *
+     * @param string $phpDoc
+     */
+    public function __construct(string $phpDoc) {
+        $this->input = $phpDoc;
+        $this->parse();
+    }
+
+    protected function parse() {
+        foreach (str::split($this->input, "\n") as $line) { // Yes. It`s works!
+            if (!str::startsWith($line, "@")) continue;
+
+            $data = str::split($line, " ", 2);
+            $this->data[str::sub($data[0], 1)] = $data[1];
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array {
+        return $this->data;
+    }
+}
